3044ebc038b05aa36b7df8b087ec11a6c51537c3
Mino Devito <mino.devito@gmail.com>
2022-06-21 10:20:16 +0100

    added feature to home page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,35 @@ import './App.css';
 
 function App() {
   return (
-    <h1>text</h1>
+    <div>
+      <div>
+        <h1> FOO-D FOR THOUGHT
+
+        </h1>
+        <p>
+          INTRODUCTION SECTION
+        </p>
+
+      </div>
+      <div>
+        <h1> WELLBEING </h1>
+        <p>
+          ABOUT SECTION
+        </p>
+        <button>
+        CLICK ME
+        </button>
+    </div>
+    <div>
+        <h1> TECHNICAL </h1>
+        <p>
+          ABOUT SECTION
+        </p>
+        <button>
+        CLICK ME
+        </button>
+      </div>
+    </div>
   );
 }
 
