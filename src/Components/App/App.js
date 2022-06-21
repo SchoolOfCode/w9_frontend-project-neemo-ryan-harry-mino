7c71bdfd89105ccbff498ab72958d4f5bdd9fe26
Ryan <highrevz@hotmail.co.uk>
2022-06-21 10:51:11 +0100

    exported home content into individual components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,35 +1,9 @@
-import './App.css';
+
+import Home from './Home/Home';
 
 function App() {
   return (
-    <div>
-      <div className='mainTitle'>
-        <h1> FOO-D FOR THOUGHT
-        </h1>
-        <p>
-          INTRODUCTION SECTION
-        </p>
-
-      </div>
-      <div className='wellbeignSection'>
-        <h1> WELLBEING </h1>
-        <p>
-          ABOUT SECTION
-        </p>
-        <button>
-        CLICK ME
-        </button>
-    </div>
-    <div className='technicalSection'>
-        <h1> TECHNICAL </h1>
-        <p>
-          ABOUT SECTION
-        </p>
-        <button>
-        CLICK ME
-        </button>
-      </div>
-    </div>
+    <Home/>
   );
 }
 
---
+++ src/Components/App/Home/Home.js
@@ -0,0 +1,23 @@
+import "./Home.css"
+const Home = () => {
+    return (
+      <div>
+        <div className="mainTitle">
+          <h1> FOO-D FOR THOUGHT</h1>
+          <p>INTRODUCTION SECTION</p>
+        </div>
+        <div className="wellbeignSection">
+          <h1> WELLBEING </h1>
+          <p>ABOUT SECTION</p>
+          <button>CLICK ME</button>
+        </div>
+        <div className="technicalSection">
+          <h1> TECHNICAL </h1>
+          <p>ABOUT SECTION</p>
+          <button>CLICK ME</button>
+        </div>
+      </div>
+    );
+}
+ 
+export default Home;
