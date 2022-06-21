4745ff3c1525bfb5602ba0d58d3e795936c27517
Mino Devito <mino.devito@gmail.com>
2022-06-21 15:46:16 +0100

    added text into techinal and  wellbeing

--- src/Components/Wellbeing/Wellbeing.js
+++ src/Components/Wellbeing/Wellbeing.js
@@ -1,6 +1,9 @@
+import Cardcomponent from "../Cardcomponent/Cardcomponent";
+
 const Wellbeing = () => {
     return ( <div>
         <h1> WELLBEING </h1>
+        <Cardcomponent title={"Wellbeing Advice"} aboutSection={"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
     </div>);
 }
  
