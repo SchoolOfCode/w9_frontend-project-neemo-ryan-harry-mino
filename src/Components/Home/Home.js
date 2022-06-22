45bba20ef1e538ee882a92769769c4f260517033
Neemo Dab <neemodab@gmail.com>
2022-06-22 12:15:39 +0100

    added useEffect Hook, fetch request working

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,33 @@
 import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from 'react'
 import "./Home.css";
 import Cardcomponent from "../Cardcomponent/Cardcomponent";
 import Footer from "../Footer/Footer";
 
 const Home = (props) => {
+  const [quote, setQuote] = useState("")
+  useEffect(() => {
+    async function fetchData() {
+      async function randomQuote() {
+        const id = Math.floor(Math.random()* 1000); 
+        const response = await fetch(`http://localhost:5432/quotes/${id.toString()}`);
+        console.log(response);
+        const data = await response.json();  
+        console.log(data);
+        setQuote(data.payload[0].text)
+      }
+      randomQuote();
+    }
+    fetchData()
+    console.log("useEffect")
+  },[])
+  
   const navigate = useNavigate();
   return (
     <div className="Home">
       <div className="title-section">
         <h1> FOO-D FOR THOUGHT</h1>
-        <p>INTRODUCTION SECTION</p>
+        <p>{quote}</p>
       </div>
       <div className="cards">
         <Cardcomponent
