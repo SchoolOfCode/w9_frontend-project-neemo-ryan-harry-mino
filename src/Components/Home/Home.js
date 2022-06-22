40e145e867f750172f2c09b01780e7d05128c5e5
Harry Evans <harryevans177@gmail.com>
2022-06-22 09:45:04 +0100

    changed components to make styling easier

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,25 +1,32 @@
 import { useNavigate } from "react-router-dom";
-import "./Home.css"
+import "./Home.css";
 import Cardcomponent from "../Cardcomponent/Cardcomponent";
 const Home = (props) => {
-const navigate = useNavigate() 
-    return (
-      <div>
-        <div className="mainTitle">
-          <h1> FOO-D FOR THOUGHT</h1>
-          <p>INTRODUCTION SECTION</p>
-        </div>
-        <div>
-          <Cardcomponent className="wellbeing-section" title={"Wellbeing"} aboutSection= {"About Section"} />
-          <button onClick={() => navigate("/wellbeing")}>CLICK ME</button>
-        </div>
-        <div>
-          <Cardcomponent className="tecnhical-section" title={"Technical"} aboutSection= {"About Section"} />
-          <button onClick={() => navigate("/technical")}>CLICK ME</button>
-        </div>
-
+  const navigate = useNavigate();
+  return (
+    <div className="Home">
+      <div className="title-section">
+        <h1> FOO-D FOR THOUGHT</h1>
+        <p>INTRODUCTION SECTION</p>
       </div>
-    );
-}
- 
+      <div className="cards">
+        <Cardcomponent
+          className="wellbeing-section"
+          title="Wellbeing"
+          aboutSection="About Section"
+          path="/wellbeing"
+          navigate={navigate}
+        />
+        <Cardcomponent
+          className="technical-section"
+          title="Technical"
+          aboutSection="About Section"
+          path="technical"
+          navigate={navigate}
+        />
+      </div>
+    </div>
+  );
+};
+
 export default Home;
