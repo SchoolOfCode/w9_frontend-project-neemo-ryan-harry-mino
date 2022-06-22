5c9803a6733bd7df00b8d9bc269dd9251c6658e8
Mino Devito <mino.devito@gmail.com>
2022-06-22 14:18:28 +0100

    brought the quote into the footer and style it

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,14 +2,32 @@ import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import Cardcomponent from "../Cardcomponent/Cardcomponent";
 import Footer from "../Footer/Footer";
+import {useState, useEffect} from "react"
 
 const Home = (props) => {
+  const [quote, setQuote] = useState("")
+  useEffect(() => {
+    async function fetchData() {
+      async function randomQuote() {
+        const id = Math.floor(Math.random()* 1000); 
+        const response = await fetch(`http://localhost:5432/quotes/${id.toString()}`);
+        console.log(response);
+        const data = await response.json();  
+        console.log(data);
+        setQuote(data.payload[0].text)
+      }
+      randomQuote();
+    }
+    fetchData()
+    console.log("useEffect")
+  },[])
+  
   const navigate = useNavigate();
   return (
     <div className="Home">
       <div className="title-section">
         <h1> FOO-D FOR THOUGHT</h1>
-        <p>INTRODUCTION SECTION</p>
+        <p>{quote}</p>
       </div>
       <div className="cards">
         <Cardcomponent
@@ -27,9 +45,11 @@ const Home = (props) => {
           navigate={navigate}
         />
       </div>
+<div className="footer-class">
       <Footer />
+      <p>{quote}</p>  
+      </div>
     </div>
   );
 };
-
 export default Home;
