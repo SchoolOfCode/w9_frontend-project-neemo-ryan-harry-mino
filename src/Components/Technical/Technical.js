395627bb7841c69f0e15624ecd84003c0beb938c
Harry Evans <harryevans177@gmail.com>
2022-06-22 15:42:48 +0100

    added styling for technical page

--- src/Components/Technical/Technical.js
+++ src/Components/Technical/Technical.js
@@ -10,38 +10,64 @@ const Technical = () => {
         <Section
           title="Frontend"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+          className="front-end-title"
         />
-        <Section title="HTML" content="Some links" className="html-section" />
-        <Section title="CSS" content="Some links" className="css-section" />
-        <Section
-          title="JavaScript"
-          content="Some links"
-          className="javascript-section"
-        />
-        <Section title="React" content="Some links" className="react-section" />
+        <div className="front-end-content">
+          <div className="front-end-left-content">
+            <Section
+              title="HTML"
+              content="Some links"
+              className="html-section"
+            />
+            <Section title="CSS" content="Some links" className="css-section" />
+          </div>
+          <div className="front-end-right-content">
+            <Section
+              title="JavaScript"
+              content="Some links"
+              className="javascript-section"
+            />
+            <Section
+              title="React"
+              content="Some links"
+              className="react-section"
+            />
+          </div>
+        </div>
       </div>
       {/* BACKEND  */}
       <div className="back-end-div">
         <Section
           title="Backend"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud "
+          className="back-end-title"
         />
-        <Section
-          title="Express"
-          content="some links"
-          className="express-section"
-        />
-        <Section title="Node" content="some links" className="node-section" />
-        <Section
-          title="PostgreSQL"
-          content="some links"
-          className="postgresql-section"
-        />
-        <Section
-          title="Heroku"
-          content="some links"
-          className="heroku-section"
-        />
+        <div className="back-end-content">
+          <div className="back-end-left-content">
+            <Section
+              title="Express"
+              content="some links"
+              className="express-section"
+            />
+            <Section
+              title="Node"
+              content="some links"
+              className="node-section"
+            />
+          </div>
+          <div className="back-end-right-content">
+            <Section
+              title="PostgreSQL"
+              content="some links"
+              className="postgresql-section"
+            />
+            <Section
+              title="Heroku"
+              content="some links"
+              className="heroku-section"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
