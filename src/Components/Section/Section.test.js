435619e0c1357d92f98ce1a370b8c59cdc85568a
Ryan <highrevz@hotmail.co.uk>
2022-06-23 10:38:51 +0100

    test for section passed

---
+++ src/Components/Section/Section.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { getByRole, render, screen } from "@testing-library/react";
+import { test, expect, jest } from "@jest/globals";
+import Section from "./Section"
+
+//this will test whether the component renders using .toBeInTheDocument matcher
+
+//Write test
+test("tests that the footer renders", function () {
+  //const addToList = jest.fn();  //not sure about this?
+  //Arrange
+  render(<Section />); //buttonText?
+  //Act
+  //do we actually need this stage for checking it renders?
+  //Assert
+  expect(screen.getByRole("heading")).toBeInTheDocument();
+});
