849555e898ece6876e2259212a72e143491ec28a
Harry Evans <harryevans177@gmail.com>
2022-06-23 16:30:08 +0100

    added link to homepage on logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <a href="https://imgur.com/FRqvO8t">
+    <a href="http://localhost:3000/">
       <img
         className="logo"
         src="https://i.imgur.com/FRqvO8t.png"
