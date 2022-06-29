9e216561cdfac5e877b43b27bca04d3b69d0dccf
Ryan <highrevz@hotmail.co.uk>
2022-06-29 11:47:56 +0100

    changed footer component to carry useeffect and fetch request logic from home,wellbeing and technical pages. this is to reduce repeated code.

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,30 @@
-import "./Footer.css"
-const Footer = () => { 
-    return (
-    <footer className="footer-component"></footer>
-    );
-}
-export default Footer
+import { useEffect, useState } from "react";
+import "./Footer.css";
+const Footer = (props) => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      async function randomQuote() {
+        const id = Math.floor(Math.random() * 1000).toString();
+        const response = await fetch(`http://localhost:5432/quotes/${id}`);
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        setQuote(data.payload[0].text);
+        setAuthor(data.payload[0].author);
+      }
+      randomQuote();
+    }
+    fetchData();
+  }, []);
+  return (
+    <footer className="footer-component">
+      <p className="quote-paragraph">
+        {quote} {author}
+      </p>
+    </footer>
+  );
+};
+export default Footer;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,31 +1,10 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 import "./Home.css";
 import Card from "../Card/Card";
 import Footer from "../Footer/Footer";
 import Logo from "../Logo/Logo";
 
 const Home = (props) => {
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
-
-  useEffect(() => {
-    async function fetchData() {
-      async function randomQuote() {
-        const id = Math.floor(Math.random() * 1000).toString();
-        const response = await fetch(`http://localhost:5432/quotes/${id}`);
-        console.log(response);
-        const data = await response.json();
-        console.log(data);
-        setQuote(data.payload[0].text);
-        setAuthor(data.payload[0].author);
-      }
-      randomQuote();
-    }
-    fetchData();
-    console.log("useEffect");
-  }, []);
-
   const navigate = useNavigate();
   return (
     <div className="Home">
@@ -51,9 +30,6 @@ const Home = (props) => {
       </div>
       <div className="footer-class">
         <Footer />
-        <p className="quote-paragraph">
-          {quote} {author}
-        </p>
       </div>
     </div>
   );
