37ee46f277d027121cfbf972fd7d29245f5645e8
Harry Evans <harryevans177@gmail.com>
2022-06-29 15:27:11 +0100

    working on testing bug found with react-router testing

--- src/Components/Card/Card.test.js
+++ src/Components/Card/Card.test.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { fireEvent, getByRole, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { test, expect, jest } from "@jest/globals";
+import { test, expect, jest, afterEach } from "@jest/globals";
+import { createMemoryHistory } from "history";
 import Home from "../Home/Home.js";
 import Wellbeing from "../Wellbeing/Wellbeing.js";
 import Technical from "../Technical/Technical.js";
 
 test("Wellbeing button routes to correct page", () => {
+  const history = createMemoryHistory();
   render(
-    <Router>
+    <Router location={history.location} navigator={history}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Wellbeing" element={<Wellbeing />} />
@@ -21,11 +23,38 @@ test("Wellbeing button routes to correct page", () => {
   const buttons = screen.getAllByRole("button");
   const wellbeingButton = buttons[0];
   const technicalButton = buttons[1];
-  expect(wellbeingButton && technicalButton).toBeInTheDocument();
+  expect(wellbeingButton).toBeInTheDocument();
+  expect(technicalButton).toBeInTheDocument();
   // clicking wellbeing button
   userEvent.click(buttons[0]);
   // has the path changed
   expect(
     screen.getByRole("heading", { name: "Overcoming Imposter Syndrome" })
+  ).toBeInTheDocument();
+});
+
+test("Technical button routes to correct page", () => {
+  const history = createMemoryHistory();
+  history.push("/some/bad/route");
+  render(
+    <Router location={history.location} navigator={history}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Wellbeing" element={<Wellbeing />} />
+        <Route path="/Technical" element={<Technical />} />
+      </Routes>
+    </Router>
+  );
+  // Checks if homepage is being rendered
+  const buttons = screen.getAllByRole("button");
+  const wellbeingButton = buttons[0];
+  const technicalButton = buttons[1];
+  expect(wellbeingButton).toBeInTheDocument();
+  expect(technicalButton).toBeInTheDocument();
+  // clicking technical button
+  userEvent.click(buttons[1]);
+  // has the path changed
+  expect(
+    screen.getByRole("heading", { name: "JavaScript" })
   ).toBeInTheDocument();
 });
